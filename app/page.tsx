98aa17e3b906d286aae643e50260dba522b51ba4
helena-varela <helena.varelakg@gmail.com>
2026-02-25 12:14:26 -0300

    finalizando projeto

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,57 @@
-import Image from "next/image";
+'use client';
+import { useEffect, useRef } from 'react';
 
 export default function Home() {
+  const ponteiroHora = useRef(null);
+  const ponteiroMinuto = useRef(null);
+  const ponteiroSegundo = useRef(null);
+
+  useEffect(() => {
+    function relogio() {
+      const now = new Date();
+      const seconds = now.getSeconds();
+      const minutes = now.getMinutes();
+      const hours = now.getHours();
+      const secondDeg = ((seconds / 60) * 360) + 90;
+      const minuteDeg = ((minutes / 60) * 360) + 90;
+      const hourDeg = ((hours / 12) * 360) + 90;
+      
+      if (ponteiroSegundo.current) {
+        ponteiroSegundo.current.style.transform = `rotate(${secondDeg}deg)`;
+      }
+      if (ponteiroMinuto.current) {
+       ponteiroMinuto.current.style.transform = `rotate(${minuteDeg}deg)`;
+      }
+      if (ponteiroHora.current) {
+        ponteiroHora.current.style.transform = `rotate(${hourDeg}deg)`;
+      }
+    }
+
+    relogio();
+
+  }, []);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div className="tela">
+      <div className="relogio">
+        <div className="relogio-numeros">
+          <div className="numero numero1">1</div>  
+          <div className="numero numero2">2</div>  
+          <div className="numero numero3">3</div>  
+          <div className="numero numero4">4</div>  
+          <div className="numero numero5">5</div>  
+          <div className="numero numero6">6</div>  
+          <div className="numero numero7">7</div>  
+          <div className="numero numero8">8</div>  
+          <div className="numero numero9">9</div>  
+          <div className="numero numero10">10</div>  
+          <div className="numero numero11">11</div>  
+          <div className="numero numero12">12</div>
+          <div ref={ponteiroHora} className="ponteiro ponteiro-hora"></div>
+          <div ref= {ponteiroMinuto} className="ponteiro ponteiro-minuto"></div>
+          <div ref={ponteiroSegundo} className="ponteiro ponteiro-segundo"></div> 
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </div>
     </div>
   );
 }
